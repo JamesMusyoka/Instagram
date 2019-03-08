3f27df7de6302d79e6c3138944ed81a15d2154f2
JamesMusyoka <jamesmu475@gmail.com>
2019-03-08 21:21:22 +0300

    Added a  url

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
 
 def  index(request):
-    return HttpResponse('Welcome to Instagram App')
+    date = dt.date.today()
+
+    
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return render(request, 'index.html',{'date':date})
+
+
+def convert_dates(dates):
+
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    day = days[day_number]
+    return day
+
+def profile(request,profile):
+    return HttpResponse(html)
