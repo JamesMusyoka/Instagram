060977c07e3eef92e09a7fd272de22490413439e
JamesMusyoka <jamesmu475@gmail.com>
2019-03-10 21:26:35 +0300

    Initialised

--- instagrid/settings.py
+++ instagrid/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'instagrid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'moringaschool',
+    'PASSWORD':'37472377',
     }
 }
 
@@ -88,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -101,3 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- instagrid/urls.py
+++ instagrid/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^instagram/',include('instagram.urls'))
+    url(r'',include('instagram.urls'))
 ]
