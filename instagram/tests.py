f86f19f03a1110d36bddeabd7f4dd9acec152837
JamesMusyoka <jamesmu475@gmail.com>
2019-03-10 21:23:14 +0300

    Added a profile test

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Image,Comment,Profile
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Image(image_name = 'James')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Image))
+
+    def test_save_method(self):
+        self.james.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def delete_image_method(self):
+        self.james.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def update_image_method(self):
+        self.james.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
