062bd73c7576762cc824fb89a6ffea18fae65805
JamesMusyoka <jamesmu475@gmail.com>
2019-03-10 21:23:26 +0300

    Initiliased

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name= 'index'),
-    url(r'^profile/$',views.profile,name = 'profile')
+    url(r'^profile/$',views.profile,name = 'profile'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
