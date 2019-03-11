bae34b63ea554f2aa860b809ad276aa580d45968
JamesMusyoka <jamesmu475@gmail.com>
2019-03-11 19:01:50 +0300

    Updated

--- instagram/models.py
+++ instagram/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='index/')
+    image = models.ImageField(upload_to = 'images/',null=True)
     image_name = models.CharField(max_length=20, default="")
     image_caption = models.TextField(max_length=150)
     # profile = models.ForeignKey(profile, null=True)
@@ -24,14 +24,30 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.profile    
+        return self.name 
+
+    def save_profile(self):
+        self.save()   
 
 
 class Comment(models.Model):
     user = models.ForeignKey(User, default=True, on_delete=models.CASCADE)
+    post = HTMLField()
     comment = models.CharField(max_length=144)
     image = models.ForeignKey(Image, on_delete=models.CASCADE, default=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_image = models.ImageField(upload_to='comment/', blank=True)
 
     def __str__(self):
         return self.comment
+
+    def save_comment(self):
+        self.save()
+
+    
+    def delete_comment(self):
+        self.delete() 
+
+class ProfileRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
