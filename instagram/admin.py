aa6be99204ca28a3cdfeebb0ad9a82da46e5ec8d
JamesMusyoka <jamesmu475@gmail.com>
2019-03-11 19:01:07 +0300

    Added a update

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Image,Comment,Profile
 
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('profile',)
+
 admin.site.register(Image)
 admin.site.register(Comment)
 admin.site.register(Profile)
